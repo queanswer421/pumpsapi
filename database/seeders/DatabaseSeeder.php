ad36ae96fc2ab074502f1939cf8e433b89c7640c
Lukas <lukasz@firma.pl>
2022-09-22 01:00:16 +0200

    add factory and migrate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\Administrator::factory(1)->create();
+        \App\Models\Company::factory(1)->create();
+        \App\Models\Employer::factory(1)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
