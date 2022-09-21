d277cde8478c78d899cacc6af73bd037aaadf757
Lukas <lukasz@firma.pl>
2022-09-22 00:40:57 +0200

    add producer category type and migrate

---
+++ app/Http/Controllers/ProducerController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Producer;
+use App\Http\Requests\StoreProducerRequest;
+use App\Http\Requests\UpdateProducerRequest;
+
+class ProducerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreProducerRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProducerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Producer  $producer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Producer $producer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProducerRequest  $request
+     * @param  \App\Models\Producer  $producer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProducerRequest $request, Producer $producer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Producer  $producer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Producer $producer)
+    {
+        //
+    }
+}
