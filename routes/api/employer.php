76a15ade9e2db56a544ad496595dea195b4b8342
Lukas <lukasz@firma.pl>
2022-09-26 06:21:14 +0200

    test

--- routes/api/employer.php
+++ routes/api/employer.php
@@ -14,9 +14,12 @@ use App\Http\Controllers\EmployerController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::get('/test',[EmployerController::class, 'index'])->name('index');
 Route::post('login',[EmployerController::class, 'login'])->name('employerLogin');
 Route::group( ['prefix' => 'emplo','middleware' => ['auth:emplo-api','scopes:emplo'] ],function(){
    // authenticated staff routes here 
     Route::get('dashboard',[EmployerController::class, 'dashboard']);
+});
+Route::get('/cars', function(){
+    return response()->json(['name' => 'Subaru', 'name2' => 'Mercedes']);
 });
